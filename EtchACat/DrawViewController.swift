f2e2430dc636c1ffcf8d8bc50890490a52b614ee
Olivia <obrown917@gmail.com>
2017-11-11 01:01:46 -0500

    Cleaner rotating buttons

--- EtchACat/DrawViewController.swift
+++ EtchACat/DrawViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class DrawViewController: UIViewController {
     
     var startPoint: CGPoint?
-
+    
     @IBOutlet weak var leftWheel: UIImageView!
     @IBOutlet weak var rightWheel: UIImageView!
     
@@ -24,32 +24,27 @@ class DrawViewController: UIViewController {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?){
-        print("hit 2")
         let touch = touches.first
+        var wheel: UIImageView = UIImageView()
         if touch!.view == leftWheel {
-            
-            let position = touch!.location(in: self.view)
-            let target = leftWheel.center
-            let angle1 = atan2(target.y-(startPoint?.y)!, target.x-(startPoint?.x)!)
-            let angle2 = atan2(target.y-position.y, target.x-position.x)
-            let angle3 = angle2-angle1
-            
-            leftWheel.transform = CGAffineTransform(rotationAngle: angle3)
+            wheel = leftWheel
         }
         else if touch!.view == rightWheel {
-            
+            wheel = rightWheel
+        }
+        
+        if wheel == leftWheel || wheel == rightWheel {
             let position = touch!.location(in: self.view)
-            let target = rightWheel.center
+            let target = wheel.center
             let angle1 = atan2(target.y-(startPoint?.y)!, target.x-(startPoint?.x)!)
             let angle2 = atan2(target.y-position.y, target.x-position.x)
             let angle3 = angle2-angle1
             
-            rightWheel.transform = CGAffineTransform(rotationAngle: angle3)
+            wheel.transform = CGAffineTransform(rotationAngle: angle3)
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("hit")
         guard touches.count == 1 else {
             return
         }
