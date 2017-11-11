39a7320f93b3d7ddd063b6f6cea0d8d33c09418c
Olivia <obrown917@gmail.com>
2017-11-11 14:16:53 -0500

    Drawing functionality

--- EtchACat/DrawViewController.swift
+++ EtchACat/DrawViewController.swift
@@ -11,12 +11,14 @@ import Alamofire
 
 class DrawViewController: UIViewController {
     
-    var startPoint: CGPoint?
-    @IBOutlet weak var lblLabel: UILabel!
+    var startAnglePoint: CGPoint?
     
     @IBOutlet weak var leftWheel: UIImageView!
     @IBOutlet weak var rightWheel: UIImageView!
     
+    @IBOutlet weak var drawingImageView: UIImageView!
+    
+    var startPoint: CGPoint? = nil
     var angleLast: CGFloat =  0.0
     
     override func viewDidLoad() {
@@ -38,7 +40,6 @@ class DrawViewController: UIViewController {
         else if touch!.view == rightWheel {
             wheel = rightWheel
         }
-        
         // return if user did not touch a wheel
         guard let rotatedWheel = wheel else {
             return
@@ -47,26 +48,51 @@ class DrawViewController: UIViewController {
         // rotate wheel
         let position = touch!.location(in: self.view)
         let target = rotatedWheel.center
-        let angleA = atan2(target.y-(startPoint?.y)!, target.x-(startPoint?.x)!)
+        let angleA = atan2(target.y-(startAnglePoint?.y)!, target.x-(startAnglePoint?.x)!)
         let angleB = atan2(target.y-position.y, target.x-position.x)
         let angleC = angleB-angleA
-        
         rotatedWheel.transform = CGAffineTransform(rotationAngle: angleC)
         
+        // check clockwise
         let angle = atan2f(Float(rotatedWheel.transform.b), Float(rotatedWheel.transform.a));
+        var isClockwise: Bool = false
         if detectClockwise(radian: CGFloat(angle)) {
-            print("Clockwise")
-        } else {
-            print("Counterclockwise")
+            isClockwise = true
         }
+        
+        // set end point of line
+        if startPoint == nil {
+            startPoint = CGPoint(x: view.frame.midX, y: view.frame.midY)
+        }
+        var endPoint: CGPoint? = nil
+        if wheel == leftWheel && isClockwise {
+            endPoint = CGPoint(x: startPoint!.x, y: startPoint!.y - 5)
+        }
+        else if wheel == leftWheel && !isClockwise {
+            endPoint = CGPoint(x: startPoint!.x, y: startPoint!.y + 5)
+        }
+        else if wheel == rightWheel && isClockwise {
+            endPoint = CGPoint(x: startPoint!.x + 5, y: startPoint!.y)
+        }
+        else if wheel == rightWheel && !isClockwise {
+            endPoint = CGPoint(x: startPoint!.x - 5, y: startPoint!.y)
+        }
+        else {
+            print("end point")
+            return // idk if necessary
+        }
+        addLine(fromPoint: startPoint!, toPoint: endPoint!)
+        startPoint = endPoint!
+        // draw very small line from last point to a slightly larger point
+        // store end point as the "to" value of the line above
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard touches.count == 1 else {
             return
         }
         if let touch = touches.first {
-            startPoint = touch.location(in: view)
+            startAnglePoint = touch.location(in: view)
         }
     }
     
@@ -77,9 +103,23 @@ class DrawViewController: UIViewController {
     @IBAction func submitPressed(_ sender: Any) {
         uploadImage()
     }
+
 }
 
 extension DrawViewController {
+    
+    func addLine(fromPoint start: CGPoint, toPoint end:CGPoint) {
+        let line = CAShapeLayer()
+        let linePath = UIBezierPath()
+        linePath.move(to: start)
+        linePath.addLine(to: end)
+        line.path = linePath.cgPath
+        line.strokeColor = UIColor.red.cgColor
+        line.lineWidth = 1
+        line.lineJoin = kCALineJoinRound
+        print("add line")
+        self.view.layer.addSublayer(line)
+    }
     
     func convertRadianToDegree(angle: CGFloat) -> CGFloat {
         var bearingDegrees = angle * (180 / .pi)
@@ -95,12 +135,12 @@ extension DrawViewController {
         var degree = self.convertRadianToDegree(angle: radian)
         degree = degree + 0.5
         
-        // moving too fast following clockwise
+        // Handles moving too fast following clockwise turn
         if angleLast > 300.0 && degree < 50 {
             angleLast = 0.0
         }
         
-        //moving too fast following counterclockwise
+        // Handles moving too fast following counterclockwise turn
         if angleLast < 100  && degree > 300 {
             angleLast = degree + 1
         }
@@ -143,4 +183,6 @@ extension DrawViewController {
             }
         }
     }
+    
+    
 }
