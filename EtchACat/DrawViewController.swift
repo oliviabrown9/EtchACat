fafc0999cfdec40cb2ba1079d65e972208ebf923
Olivia <obrown917@gmail.com>
2017-11-11 09:57:44 -0500

    Detect clockwise/counterclockwise

--- EtchACat/DrawViewController.swift
+++ EtchACat/DrawViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class DrawViewController: UIViewController {
     
     var startPoint: CGPoint?
+    @IBOutlet weak var lblLabel: UILabel!
     
     @IBOutlet weak var leftWheel: UIImageView!
     @IBOutlet weak var rightWheel: UIImageView!
+    
+    var angleLast: CGFloat =  0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +49,15 @@ class DrawViewController: UIViewController {
         let angle1 = atan2(target.y-(startPoint?.y)!, target.x-(startPoint?.x)!)
         let angle2 = atan2(target.y-position.y, target.x-position.x)
         let angle3 = angle2-angle1
+        
         rotatedWheel.transform = CGAffineTransform(rotationAngle: angle3)
+        
+        let angle = atan2f(Float(rotatedWheel.transform.b), Float(rotatedWheel.transform.a));
+        if detectClockwise(radian: CGFloat(angle)) {
+            print("Clockwise")
+        } else {
+            print("Counterclockwise")
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -56,6 +67,55 @@ class DrawViewController: UIViewController {
         if let touch = touches.first {
             startPoint = touch.location(in: view)
         }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        angleLast = 0
+    }
+}
+
+extension DrawViewController {
+    
+    func convertRadianToDegree(angle: CGFloat) -> CGFloat {
+        var bearingDegrees = angle * (180 / .pi)
+        if bearingDegrees > 0.0 {
+            
+        } else {
+            bearingDegrees =  bearingDegrees + 360
+        }
+        return CGFloat(bearingDegrees)
+    }
+    
+    func detectClockwise(radian: CGFloat) -> Bool {
+        var degree = self.convertRadianToDegree(angle: radian)
+        degree = degree + 0.5
+        
+        // moving too fast following clockwise
+        if angleLast > 300.0 && degree < 50 {
+            angleLast = 0.0
+        }
+        
+        //moving too fast following anti-clockwise
+        if angleLast < 100  && degree > 300 {
+            angleLast = degree + 1
+        }
+        
+        var returnData = false
+        if angleLast <= degree  {
+            angleLast = degree
+            returnData = true
+        } else {
+            angleLast = degree
+            returnData = false
+        }
+        
+        return returnData
+    }
+}
+
+extension DrawViewController {
+    func radionToDegree(_ radian: CGFloat) -> CGFloat {
+        return radian * (180 / .pi)
     }
 }
 
